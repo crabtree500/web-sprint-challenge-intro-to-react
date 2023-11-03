1787433376c9bdd4c4c44be70328c37c3d162381
Brad <crabtree500@gmail.com>
2023-11-02 22:44:50 -0700

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,45 @@
-import React from 'react'
-import axios from 'axios'
-import Character from './Character'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './Character';
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPlanets = 'http://localhost:9009/api/planets';
+const urlPeople = 'http://localhost:9009/api/people';
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
+  // Create state to hold the data from the API
+  const [name, setName] = useState('');
+  const [homeworld, setHomeworld] = useState('');
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [peopleResponse, planetsResponse] = await Promise.all([
+          axios.get(urlPeople),
+          axios.get(urlPlanets),
+        ]);
+
+        const person = peopleResponse.data;
+        const planet = planetsResponse.data;
+
+        setName(person.name);
+        setHomeworld(person.homeworld);
+        
+        const matchedData = [];
+        if (person.homeworld === planet.id) {
+          matchedData.push({
+            homeworld: planet.name,
+          });
+        }
+        
+        setMatches(matchedData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <h2>Star Wars Characters</h2>
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 function Character() { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [homeworldclass, setHomeworldclass] = usestate('')
   // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className='character-card'>
+      <h3 className='character-name'>placeholder</h3>
+      <p> "Planet: "
+        <span className='character-planet'>placeholder</span>
+      </p>
     </div>
   )
 }
